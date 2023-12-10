242c4c61638eccc37e85c9c0470d0a6c5cd84ada
kunakorn topurin <97731297+Bright-Kunakorn@users.noreply.github.com>
2023-12-10 10:36:24 +0700

    Update brand_controller.go

--- pkg/brand/controller/brand_controller.go
+++ pkg/brand/controller/brand_controller.go
@@ -88,6 +88,9 @@ func (controller *BrandController) FindById(ctx *gin.Context) {
 	ctx.JSON(http.StatusOK, webResponse)
 }
 
+
+
+
 // FindAllBrand 	godoc
 // @Summary			Get All brand.
 // @Description		Return list of brand.
